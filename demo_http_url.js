c6681e1f8fdd43dc74c2f7d004a2c2bbfce19465
SalumJS Ulomi <abuunuaym2013@gmail.com>
2020-02-03 20:56:29 +0300

    anothe

--- demo_http_url.js
+++ demo_http_url.js
@@ -1,9 +1,19 @@
 var http = require('http');
 var fs = require('fs');
+
 http.createServer((req, res) => {
-  fs.readFile('demo_html.html', (err, data) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
-  });
+  var data = `<html>
+  <body>
+     <h1 style = "color:red">Hello Serengeti</h1>
+     <p>The Big National Park in the World</p>
+  </body>
+  </html>`;
+  fs.appendFile('myfile.html', data, (err) => {
+    if (err) throw err;
+    else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    }
+  })
 }).listen(4000);
