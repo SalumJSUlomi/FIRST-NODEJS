5b95cbaff94c8c1b9a7b70b058478d76c9d25051
SalumJS Ulomi <abuunuaym2013@gmail.com>
2020-02-05 13:53:43 +0300

    query

--- demo_http_url.js
+++ demo_http_url.js
@@ -1,5 +1,13 @@
-var fs = require('fs');
-fs.unlink('demo_html.html', (err) => {
-  if (err) throw err;
-  console.log('File Deleted...')
-});
+var url = require('url');
+
+var addr = 'http://localhost:8000/default.html?year=2020&month=february';
+var q = url.parse(addr, true);
+console.log(q);
+console.log(q.host);
+console.log(q.hostname);
+console.log(q.pathname);
+console.log(q.search);
+
+var qdata = q.query;
+console.log(qdata);
+console.log(qdata.year);
