b4eb151407780b0103ffe1707c69afdade5fbbc2
SalumJS Ulomi <abuunuaym2013@gmail.com>
2020-02-09 19:22:10 +0300

    foridable

--- README.md
+++ README.md
@@ -652,3 +652,4 @@ http.createServer((req, res)=> {
    }
  }).listen(8000);
  ```
+ 
--- upload.js
+++ upload.js
@@ -1,18 +1,24 @@
 var http = require('http');
 var formidable = require('formidable');
+var fs = require('fs');
+
 http.createServer((req, res) => {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
     form.parse(req, (err, fields, files) => {
-      res.write('File Uploaded');
-      res.end();
-    })
+      var oldpath = files.filetoupload.path;
+      var newpath = 'C:/Users/USER/Desktop/nodejs/img/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, (err) => {
+        if (err) throw err;
+        res.write('File Uploaded and Moved');
+        res.end();
+      });
+    });
   } else {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<form action = "fileupload" method = "post" enctype = "multipart/form-data">');
     res.write('<input type = "file" name = "filetoupload" required><br>');
     res.write('<input type = "submit">');
-    res.write('</form>');
     return res.end();
   }
-}).listen(5000);
+}).listen(7000);
