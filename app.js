be421db8174b550a10de69ee5c1cf0de4b9640f2
SalumJS Ulomi <abuunuaym2013@gmail.com>
2020-02-09 11:36:23 +0300

    emit

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var fs = require('fs');
-var rs = fs.createReadStream('./demofile.txt');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
-rs.on('open', () => {
-  console.log('The file is open');
-});
+// Create an event handler
+var myEventHandler = function() {
+  console.log('I hear a scream');
+}
+// Assign the event handler to an event
+eventEmitter.on('scream', myEventHandler);
+// Fire the scream event 
+eventEmitter.emit('scream');
